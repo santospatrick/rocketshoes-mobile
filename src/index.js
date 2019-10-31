efcbfa6000541c312ce583f96a5426680aa838ed
Patrick Santos <patrickspteixeira@gmail.com>
2019-10-31 20:58:16 -0300

    adiciona lib para icones

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const App = () => {
     return (
         <View
             style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Rocketshoes</Text>
+            <Icon name="shopping-basket" size={24} color="#000" />
         </View>
     );
 };
